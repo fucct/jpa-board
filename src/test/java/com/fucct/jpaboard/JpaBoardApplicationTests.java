fe4ab551e786c5a4289b199ab2947a605193e79c
dd <dqrd123@gmail.com>
2020-10-21 19:19:14 +0900

    Initial commit

---
+++ src/test/java/com/fucct/jpaboard/JpaBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fucct.jpaboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
